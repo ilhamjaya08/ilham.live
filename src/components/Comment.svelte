e371eaa1d88410500e65b4aebd015d37e30ebc1a
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-09-03 11:28:37 +0700

    feat: Fragment and Comment components & bio section

---
+++ src/components/Comment.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let name;
+</script>
+
+<div class="flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+        <path fill="currentColor" fill-rule="evenodd" d="M17.5 17.5L9.25 12l8.25-5.5l-1-1.5L6 12l10.5 7z" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" {...$$props}>
+        <g fill="currentColor">
+            <g opacity="0.2">
+                <path fill-rule="evenodd" d="M11 3a2 2 0 0 1 2 2v7a2 2 0 1 1-4 0V5a2 2 0 0 1 2-2" clip-rule="evenodd" />
+                <path d="M13 17a2 2 0 1 1-4 0a2 2 0 0 1 4 0" />
+            </g>
+            <path fill-rule="evenodd" d="M10.25 2.25A.75.75 0 0 1 11 3v9a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+            <path d="M11.5 16.25a1.25 1.25 0 1 1-2.5 0a1.25 1.25 0 0 1 2.5 0" />
+        </g>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 72 72" {...$$props}>
+        <path d="M59 41H13V31h46" />
+        <path fill="#3f3f3f" d="M59 41H13V31h46" />
+        <path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M13 31h46v10H13z" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 72 72" {...$$props}>
+        <path d="M59 41H13V31h46" />
+        <path fill="#3f3f3f" d="M59 41H13V31h46" />
+        <path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M13 31h46v10H13z" />
+    </svg>
+    <span class='font-suse'>{name}</span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 72 72" {...$$props}>
+        <path d="M59 41H13V31h46" />
+        <path fill="#3f3f3f" d="M59 41H13V31h46" />
+        <path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M13 31h46v10H13z" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 72 72" {...$$props}>
+        <path d="M59 41H13V31h46" />
+        <path fill="#3f3f3f" d="M59 41H13V31h46" />
+        <path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M13 31h46v10H13z" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+        <path fill="currentColor" fill-rule="evenodd" d="m6.5 17.5l8.25-5.5L6.5 6.5l1-1.5L18 12L7.5 19z" />
+    </svg>
+</div>
