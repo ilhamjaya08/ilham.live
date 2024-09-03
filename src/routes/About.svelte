2eed2d716d4aa061a4e3c6acfc791f27dc796aa8
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-09-03 09:36:34 +0700

    feat: sticky navbar

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<article id='about' class='bg-primary py-12 text-white'>
+<article id='about' class='bg-[#6c63ff] py-12 text-white'>
     <div class='bg-base-200 px-4 mx-auto w-full md:max-w-3xl lg:max-w-6xl'>
         <h1>
             <span class='text-3xl font-bold text-center'>About Me</span>
