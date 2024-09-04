f40dd1b397cb0974f9a69e13c5922b196bfec6eb
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-09-04 10:26:26 +0700

    feat: skillset page

---
+++ src/components/StackItemCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+
+    export let name: string;
+    export let tools: { icon: any; name: string }[] = [];
+    export let wide = false;
+</script>
+
+<article class="p-4 rounded-md border-base-content border-2 space-y-4 md:h-full transition">
+  <header class="text-center">
+    <p class="font-bold font-suse">{name}</p>
+  </header>
+  <div>
+    <ul class="flex flex-row flex-wrap">
+      {#each tools as tool}
+        <li class={`basis-1/2 ${wide ? 'md:basis-1/3 lg:basis-1/4' : 'md:basis-full lg:basis-1/2'}`}>
+          <div class="p-2 space-x-4 cursor-default group">
+            <Icon icon={tool.icon} class="inline-block w-6 h-6 md:w-8 md:h-8" />
+            <p class="font-suse inline-block decoration-accent group-hover:underline font-bold text-xsm">{tool.name}</p>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</article>
--- src/routes/SkillSet.svelte
+++ src/routes/SkillSet.svelte
@@ -1,6 +1,46 @@
 <script lang="ts">
     import Fragment from "../components/Fragment.svelte";
     import Comment from "../components/Comment.svelte";
+    import StackItemCard from "../components/StackItemCard.svelte";
+    const web = [
+    { name: 'HTML5', icon: 'logos:html-5' },
+    { name: 'CSS3', icon: 'logos:css-3' },
+    { name: 'Svelte', icon: 'devicon:svelte' },
+    { name: 'Astro', icon: 'devicon:astro' },
+    { name: 'React', icon: 'logos:react' },
+    { name: 'NextJS', icon: 'logos:nextjs-icon' },
+    { name: 'Tailwind', icon: 'logos:tailwindcss-icon' },
+    ];
+    const be = [
+    { name: 'NodeJS', icon: 'logos:nodejs' },
+    { name: 'Bun', icon: 'logos:bun' },
+    { name: 'Laravel', icon: 'logos:laravel' },
+    { name: 'Express', icon: 'devicon:express' },
+    { name: 'NestJS', icon: 'devicon:nestjs' },
+    { name: 'Hono', icon: 'logos:hono' }
+    ]
+    const lang = [
+    { name: 'Typescript', icon: 'devicon:typescript' },
+    { name: 'JavaScript', icon: 'logos:javascript' },
+    { name: 'Java', icon: 'logos:java' },
+    { name: 'PHP', icon: 'logos:php' },
+    ];
+    const infra = [
+    { name: 'Actions', icon: 'devicon:githubactions' },
+    { name: 'Docker', icon: 'devicon:docker' },
+    { name: 'Vercel', icon: 'devicon:vercel' },
+    { name: 'cPanel', icon: 'logos:cpanel' },
+    { name: 'Linux', icon: 'devicon:linux' },
+    { name: 'Heroku', icon: 'devicon:heroku' },
+    { name: 'AWS', icon: 'logos:aws' },
+    ];
+    const db = [
+    { name: 'MySQL', icon: 'logos:mysql' },
+    { name: 'MongoDB', icon: 'devicon:mongodb' },
+    { name: 'PostgreSQL', icon: 'logos:postgresql' },
+    { name: 'Prisma', icon: 'logos:prisma' },
+    { name: 'TypeOrm', icon: 'logos:typeorm' },
+    ];
 </script>
 
 <article id='skillset' class='bg-[#ffffff] py-8'>
@@ -8,5 +48,22 @@
         <h1 class='font-suse font-extrabold text-3xl'>
             <Fragment name='SkillSet' />
         </h1>
+        <ul class='mt-6 flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-x-0 lg:justify-center'>
+            <li class="md:basis-1/2 md:px-2">
+                <StackItemCard name='Web Development' tools={web} wide={true}  />
+            </li>
+            <li class="md:basis-1/2 md:px-2">
+                <StackItemCard name='Backend Server' tools={be} wide={true}  />
+            </li>
+            <li class="md:basis-1/2 md:px-2">
+                <StackItemCard name='Programming Languages' tools={lang} wide={true}  />
+            </li>
+            <li class="md:basis-1/2 md:px-2">
+                <StackItemCard name='Infrastructure, CI/CD & DevOps' tools={infra} wide={true}  />
+            </li>
+            <li class="md:basis-1/2 md:px-2">
+                <StackItemCard name='Databases & ORM' tools={db} wide={true}  />
+            </li>
+        </ul>
     </div>
 </article>
